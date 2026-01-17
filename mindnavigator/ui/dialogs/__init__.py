d6c3e0016faf88c9792abf9ab7b697be69f33343
Lex <lexflame@gmail.com>
2026-01-17 16:32:30 +0700

    feat: add file workspace attachment selector

--- mindnavigator/ui/dialogs/__init__.py
+++ mindnavigator/ui/dialogs/__init__.py
@@ -1,1 +1,4 @@
-"""Dialog widgets for MindNavigator."""
+from .attach_file_select_nav import AttachFileSelectNav
+from .map_label_edit_dialog import MapLabelEditDialog
+
+__all__ = ["AttachFileSelectNav", "MapLabelEditDialog"]
